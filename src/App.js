1f0324613297478049420e36a148908dcb226ee9
mapper895 <mapper.895.mp@gmail.com>
2021-03-11 12:14:36 -0600

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
