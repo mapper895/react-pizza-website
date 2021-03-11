3fad7f8dd8521ff9e8fed282b9ff5aa2eb39eb71
mapper895 <mapper.895.mp@gmail.com>
2021-03-10 19:30:54 -0600

    Global Styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import { GlobalStyle } from "./GlobalStyles";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
-    </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import { GlobalStyle } from "./GlobalStyles";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <Router>
       <App />
     </Router>
